e77872d586303ae2852dcbc00463e8c78f971113
avni25 <huseyinac@gmail.com>
2022-03-24 16:11:54 +0300

    ...

--- app.py
+++ app.py
@@ -5,27 +5,6 @@ from PyQt5.QtGui import QIcon
 from MainWindow import Ui_MainWindow
 
 
-# class MyWindow(QMainWindow):
-#     def __init__(self):
-#         super(MyWindow, self).__init__()
-#         self.setWindowTitle("AutoTwitter")
-#         self.setGeometry(300,300,500,500)
-#         self.setWindowIcon(QIcon("twitter.png"))
-#         self.setToolTip("qwe")    
-#         self.initUI()
-        
-#     def initUI(self):
-#         self.label_name = QtWidgets.QLabel(self)
-#         self.label_name.setText("name")
-
-# def window():
-#     app = QtWidgets.QApplication(sys.argv)
-#     win = MyWindow()  
-    
-#     win.show()
-#     sys.exit(app.exec_())
-    
-# window()
 
 class App(QtWidgets.QMainWindow):
     def __init__(self):
