16a6c18ae2015210b2d01d8e2bab74c1dad5dd1a
avni25 <huseyinac@gmail.com>
2022-03-24 16:11:38 +0300

    ...

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QToolTip
 from PyQt5.QtGui import QIcon
+from MainWindow import Ui_MainWindow
 
 
-def window():
+# class MyWindow(QMainWindow):
+#     def __init__(self):
+#         super(MyWindow, self).__init__()
+#         self.setWindowTitle("AutoTwitter")
+#         self.setGeometry(300,300,500,500)
+#         self.setWindowIcon(QIcon("twitter.png"))
+#         self.setToolTip("qwe")    
+#         self.initUI()
+        
+#     def initUI(self):
+#         self.label_name = QtWidgets.QLabel(self)
+#         self.label_name.setText("name")
+
+# def window():
+#     app = QtWidgets.QApplication(sys.argv)
+#     win = MyWindow()  
+    
+#     win.show()
+#     sys.exit(app.exec_())
+    
+# window()
+
+class App(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(App, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+def app():
     app = QtWidgets.QApplication(sys.argv)
-    win = QMainWindow()
-    
-    win.setWindowTitle("AutoTwitter")
-    win.setGeometry(300,300,500,500)
-    win.setWindowIcon(QIcon("twitter.png"))
-    
+    win = App()
     win.show()
     sys.exit(app.exec_())
-    
-window()
 
+app()    
 
 
 
