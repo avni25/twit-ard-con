d6264df9b3d8dd5e92eb474036141e534a64017a
avni25 <huseyinac@gmail.com>
2022-03-24 11:05:00 +0300

    ..

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ import tweepy
 from config import *
 
 
-auth = tweepy.OAuth1UserHandler(
-   API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-)
+# auth = tweepy.OAuth1UserHandler(
+#    API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+# )
+
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
 public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+# for tweet in public_tweets:
+#     print(tweet.text)
 
 # Get the User object for twitter...
 user = api.get_user(screen_name='havni25')
-print("user screen name: "+user.screen_name)
-print('followers: {0}'.format(user.followers_count))
-print('following: {0}'.format(user.friends_count))
-for friend in user.friends():
-   print(friend.screen_name)
+print(user.screen_name)
+print(user.followers_count)
+print(user.friends_count)
+print("---------------------")
+# for friend in user.friends():
+#    print(friend.screen_name)
+
+tweet = "#how i met your mother"
+
+api.update_status(tweet)
+
